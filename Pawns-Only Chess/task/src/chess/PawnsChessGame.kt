28aaae7f738893f3a83c0c03b451971215625fcc
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-08-07 17:16:48 +0900

    Stage 4/5: Capture

--- Pawns-Only Chess/task/src/chess/PawnsChessGame.kt
+++ Pawns-Only Chess/task/src/chess/PawnsChessGame.kt
@@ -12,6 +12,7 @@ class PawnsChessGame(private val playerOne: String, private val playerTwo: Strin
         "        ".toCharArray()
     )
     var currentPlayer: String = playerOne
+    private var lastMove = ""
 
     fun showBoard() {
         for (i in 8 downTo 1) {
@@ -42,10 +43,7 @@ class PawnsChessGame(private val playerOne: String, private val playerTwo: Strin
             println("Invalid Input")
             return false
         }
-        val x1 = "87654321".indexOf(move[1])
-        val y1 = "abcdefgh".indexOf(move[0])
-        val x2 = "87654321".indexOf(move[3])
-        val y2 = "abcdefgh".indexOf(move[2])
+        val (x1, y1, x2, y2) = getGridCoordinates(move)
         System.err.println("$move: $x1,$y1 -> $x2,$y2")
 
         if (currentPlayer == playerOne) {
@@ -53,13 +51,28 @@ class PawnsChessGame(private val playerOne: String, private val playerTwo: Strin
                 println("No white pawn at ${move[0]}${move[1]}")
                 return false
             }
-            if (y1 != y2 ||
-                x1 - x2 > 2 ||
-                x1 - x2 < 0 ||
-                x1 != 6 && x2 != x1 - 1 ||
-                grid[x2][y2] != ' ') {
-                println("Invalid Input")
-                return false
+            if (grid[x2][y2] == 'B' || isPassantMove(lastMove) && isTakingPassantPawn(move, lastMove)) {
+                System.err.println("try to capture figure at ${move[2]}${move[3]} ($x1, $y1) -> ($x2, $y2)")
+                if (x1 - x2 != 1 ||
+                    kotlin.math.abs(y2 - y1) != 1
+                ) {
+                    println("Invalid Input")
+                    return false
+                }
+                if (isPassantMove(lastMove) && isTakingPassantPawn(move, lastMove)) {
+                    val (_, _, x2, y2) = getGridCoordinates(lastMove)
+                    grid[x2][y2] = ' '
+                }
+            } else {
+                if (y1 != y2 ||
+                    x1 - x2 > 2 ||
+                    x1 - x2 < 0 ||
+                    x1 != 6 && x2 != x1 - 1 ||
+                    grid[x2][y2] != ' '
+                ) {
+                    println("Invalid Input")
+                    return false
+                }
             }
             grid[x2][y2] = 'W'
         }
@@ -69,18 +82,55 @@ class PawnsChessGame(private val playerOne: String, private val playerTwo: Strin
                 println("No black pawn at ${move[0]}${move[1]}")
                 return false
             }
-            if (y1 != y2 ||
-                x2 - x1 > 2 ||
-                x2 - x1 < 0 ||
-                x1 != 1 && x2 != x1 + 1 ||
-                grid[x2][y2] != ' ') {
-                println("Invalid Input")
-                return false
+            if (grid[x2][y2] == 'W' || isPassantMove(lastMove) && isTakingPassantPawn(move, lastMove)) {
+                System.err.println("try to capture figure at ${move[2]}${move[3]} ($x1, $y1) -> ($x2, $y2)")
+                if (x2 - x1 != 1 ||
+                    kotlin.math.abs(y2 - y1) != 1
+                ) {
+                    println("Invalid Input")
+                    return false
+                }
+                if (isPassantMove(lastMove) && isTakingPassantPawn(move, lastMove)) {
+                    val (_, _, x2, y2) = getGridCoordinates(lastMove)
+                    grid[x2][y2] = ' '
+                }
+            } else {
+                if (y1 != y2 ||
+                    x2 - x1 > 2 ||
+                    x2 - x1 < 0 ||
+                    x1 != 1 && x2 != x1 + 1 ||
+                    grid[x2][y2] != ' '
+                ) {
+                    println("Invalid Input")
+                    return false
+                }
             }
             grid[x2][y2] = 'B'
         }
+        lastMove = move
         grid[x1][y1] = ' '
         currentPlayer = if (currentPlayer == playerOne) playerTwo else playerOne
         return true
+    }
+
+    private fun isTakingPassantPawn(move: String, lastMove: String): Boolean {
+        val (_, _, mx2, my2) = getGridCoordinates(move)
+        val (lmx1, _, lmx2, lmy2) = getGridCoordinates(lastMove)
+        return lmy2 == my2 && mx2 == (lmx2 + lmx1) / 2
+    }
+
+    private fun getGridCoordinates(move: String): Array<Int> {
+        val x = "87654321"
+        val y = "abcdefgh"
+        return arrayOf(x.indexOf(move[1]),
+            y.indexOf(move[0]),
+            x.indexOf(move[3]),
+            y.indexOf(move[2]))
+    }
+
+    private fun isPassantMove(move: String): Boolean {
+        if (move.isEmpty()) return false
+        val (x1, y1, x2, y2) = getGridCoordinates(move)
+        return (kotlin.math.abs(x2 - x1) == 2 && y2 == y1)
     }
 }
